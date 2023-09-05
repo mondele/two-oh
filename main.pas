0699a42335b7df0e1bd84e13c1145f8f8e5f778c
John <john_wood@wycliffeassociates.org>
2023-09-05 07:50:56 -0400

    initial form layout

---
+++ main.pas
@@ -0,0 +1,32 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Shape1: TShape;
+    Shape2: TShape;
+    StatusBar1: TStatusBar;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
