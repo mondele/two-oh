b1420009ce31c8779981a84591f7b104f113019c
mondele <John_Wood@WycliffeAssociates.org>
2024-08-27 20:37:14 -0400

    More changes, too many to count

--- main.pas
+++ main.pas
@@ -5,18 +5,21 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls, DefaultTranslator;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls,
+  DefaultTranslator, StdCtrls;
 
 type
 
   { Tdashboard }
 
   Tdashboard = class(TForm)
+    Label1: TLabel;
     Panel1: TPanel;
     SideBar: TShape;
     titleBar: TShape;
     StatusBar1: TStatusBar;
     procedure FormCreate(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
     procedure titleBarChangeBounds(Sender: TObject);
     procedure SideBarChangeBounds(Sender: TObject);
   private
@@ -40,6 +43,11 @@ begin
 end;
 
 procedure Tdashboard.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure Tdashboard.Label1Click(Sender: TObject);
 begin
 
 end;
